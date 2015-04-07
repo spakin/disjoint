a5d612ff8bedcdae17f7c37ee72fa7be37f207fc
Scott Pakin <scott+gpg@pakin.org>
2015-04-07 00:01:59 -0600

    Bug fix: Corrected even/odd test

--- disjoint_test.go
+++ disjoint_test.go
@@ -19,23 +19,23 @@ func TestEvenOdd(t *testing.T) {
 
 	// Merge each even number with its predecessor and each odd number with
 	// its predecessor.
-	for i := 2; i < N; i++ {
+	for i := 2; i < N; i += 2 {
 		sets[i].Merge(sets[i-2])
 	}
-	for i := 3; i < N; i++ {
+	for i := 3; i < N; i += 2 {
 		sets[i].Merge(sets[i-2])
 	}
 
 	// Ensure that even numbers are in the same union as other even numbers
 	// and odd numbers are in the same union as other oddn numbers.
-	for i := 0; i < N*2; i++ {
+	for i := 0; i < N*3; i++ {
 		s1 := rand.Intn(N)
 		s2 := rand.Intn(N)
 		sameMod2 := s1%2 == s2%2
 		sameRep := sets[s1].Representative() == sets[s2].Representative()
 		if sameMod2 != sameRep {
-			t.Fatalf("Are %d and %d in the same set?  The package incorrectly says %v",
-				sets[s1], sets[s2], sameRep)
+			t.Fatalf("Should %d and %d lie in the same set?  The package incorrectly says %v.",
+				s1, s2, sameRep)
 		}
 	}
 }
