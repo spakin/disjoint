5432596f48405215b820845a525678a542c65e53
Scott Pakin <scott+gpg@pakin.org>
2015-04-10 01:15:39 -0600

    Major documentation rewrite

--- disjoint.go
+++ disjoint.go
@@ -12,35 +12,49 @@ elements.
 • Find returns an arbitrary element from a set.
 
 The critical feature is that Find returns the same element when given any
-element in a set.  The implication is that two sets A and B belong to the same
-union if and only if A.Find() == B.Find().
+element in a set.  The implication is that two elements A and B belong to the
+same set if and only if A.Find() == B.Find().
+
+Disjoint sets are more limited in functionality than conventional sets.  They
+support only set union, not set intersection, set difference, or any other set
+operation.  They don't allow an element to reside in more than one set.  They
+don't even provide a way to enumerate the elements in a given set.  What makes
+them useful, though, is that they're extremely fast, especially for large sets;
+both Union and Find run in amortized near-constant time.  See
+http://en.wikipedia.org/wiki/Disjoint_set_forest for more information.
 
 Disjoint sets are often used in graph algorithms, for example to find a minimal
 spanning tree for a graph or to determine if adding a given edge to a graph
 would create a cycle.
-
-Both Union and Find run in amortized near-constant time.  See
-http://en.wikipedia.org/wiki/Disjoint_set_forest for more information.
 */
 package disjoint
 
-// An Element represents a value that can be associated with a set of other
-// values.
+// An Element represents a single element of a set.
+//
+// The Data field lets a program store arbitrary data within an Element.  This
+// can be used, for example, to keep track of the total number of elements in
+// the associated set, the set's maximum-valued element, a map of attributes
+// associated with the set's elements, or even a map or slice of all elements
+// in the set.  (That last possibility would associate a linear-time cost with
+// each Union operation but would not affect Find's near-constant running
+// time.)
 type Element struct {
-	parent *Element // Parent element
-	rank   int      // Rank (approximate depth) of the subtree with this element as root
+	parent *Element    // Parent element
+	rank   int         // Rank (approximate depth) of the subtree with this element as root
+	Data   interface{} // Arbitrary user-provided payload
 }
 
-// NewElement returns an element that is not associated with any other element.
+// NewElement returns an element belonging to a singleton set.
 func NewElement() *Element {
 	s := &Element{rank: 0}
 	s.parent = s
 	return s
 }
 
-// Find returns an arbitrary Element belonging to a union of Elements.  The
-// important feature is that it returns the same Element for any Element in the
-// union so it can be used to test if two Elements are in the same union.
+// Find returns an arbitrary Element from a set containing a given Element.
+// The important feature is that it returns the same value for every element in
+// the set.  Consequently, it can be used to test if two Elements belong to the
+// same set.
 func (s *Element) Find() *Element {
 	for s.parent != s {
 		s.parent = s.parent.parent
@@ -49,9 +63,13 @@ func (s *Element) Find() *Element {
 	return s
 }
 
-// Union associates two Elements—and by transitivity, all Elements with which
-// they are associated.  Thenceforth, Find will return the same Element when
-// given any Element in the union.
+// Given an element from each of two sets, Union performs a destructive union
+// of those sets.  Afterwards, the original sets no longer exist as separate
+// entities.
+//
+// Note that, perhaps surprisingly, the package does not expose an explicit Set
+// data type.  Sets exist only implicitly based on the sequence of Union
+// operations performed on their elements.
 func (s *Element) Union(t *Element) {
 	// Ensure the two Elements aren't already part of the same union.
 	sRoot := s.Find()
